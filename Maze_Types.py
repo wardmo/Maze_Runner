f756915b4163ccc8f7d319f8b635794f8a55638f
Morgan Ward <wardmo.42@gmail.com>
2023-02-19 23:02:08 -0700

    I might keep these as well

--- Maze_Types.py
+++ Maze_Types.py
@@ -5,3 +5,15 @@ class Maze_Types(Enum):
     Path = 1
     Start = 2
     End = 3
+
+class Maze_Node():
+    def __init__(self, id: tuple[int,int]):
+        self.data = None
+        self.id: tuple[int,int] = id
+        self.connected_nodes: list[Maze_Node] = []
+
+class Maze_Edge():
+    def __init__(self, id: tuple[tuple[int,int], tuple[int,int]]):
+        self.type: Maze_Types = Maze_Types.Wall
+        self.id: tuple[tuple[int,int], tuple[int,int]] = id
+        self.reverse_id: tuple[tuple[int,int],tuple[int,int]] = (id[1],id[0])
