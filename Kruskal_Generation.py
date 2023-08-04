0ed9c97548f42b5c4f0ba7a298b4f321cb72a139
wardmo <54964980+wardmo@users.noreply.github.com>
2023-08-04 15:49:33 -0600

    Update Kruskal_Generation.py
    fixed a problem with generating 2x edges, generated a couple mazes and verified by hand they are right. Next lets work on a visualizer

--- Kruskal_Generation.py
+++ Kruskal_Generation.py
@@ -1,52 +1,51 @@
 from random import shuffle
 
-
 class Kruskal_Generation():
-    def __init__(self, height, width):
-        nodes = set()
-        edges = set()
+    @classmethod
+    def generate(cls, height, width):
+        return Kruskal_Generation(height, width).solution
 
+    def __init__(self, height, width):
+        self.nodes = set()
+        self.edges = set()
+
+        # populate all the nodes and edges
         for j in range(height):
             for i in range(width):
-                current_node = (i,j)
-                nodes.add(current_node)
-                if i > 0:
-                    neighbor_node = (i-1,j)
-                    edges.add((current_node,neighbor_node))
-                if i < width - 1:
-                    neighbor_node = (i+1,j)
-                    edges.add((current_node,neighbor_node))
-                if j > 0:
-                    neighbor_node = (i,j-1)
-                    edges.add((current_node,neighbor_node))
-                if j < height - 1:
-                    neighbor_node = (i,j+1)
-                    edges.add((current_node,neighbor_node))
-        
-        clusters = {n:n for n in nodes}
-        ranks = {n:0 for n in nodes}
-        solution = set()
-        edges = list(edges)
-        shuffle(edges)
+                current_node = (j,i)
+                self.nodes.add(current_node)
+                if i < width-1:
+                    self.edges.add((current_node, (j, i+1)))
+                if j < height-1:
+                    self.edges.add((current_node, (j+1, i)))
 
-        def find_set(n):
-            if clusters[n] != n:
-                clusters[n] = find_set(clusters[n])
-            return clusters[n]
+        self.clusters = {n:n for n in self.nodes}
+        self.ranks = {n:0 for n in self.nodes}
+        solution = set()
+        self.edges = list(self.edges)
+        shuffle(self.edges)
         
-        def union(n1,n2):
-            x,y = find_set(n1), find_set(n2)
-            if ranks[x] == ranks[y]:
-                clusters[y] = x
-            else:
-                clusters[x] = y
-            if ranks[x] == ranks[y]:
-                ranks[y] += 1
-        
-        for n1,n2 in edges:
+        for n1,n2 in self.edges:
             if n1 != n2:
-                if find_set(n1) != find_set(n2):
+                if self.find_set(n1) != self.find_set(n2):
                     solution.add((n1,n2))
-                    union(n1,n2)
+                    self.union(n1,n2)
 
         self.solution = solution
+        pass
+
+    def find_set(self, n):
+        if self.clusters[n] != n:
+            self.clusters[n] = self.find_set(self.clusters[n])
+        return self.clusters[n]
+        pass
+    
+    def union(self,n1,n2):
+        x,y = self.find_set(n1), self.find_set(n2)
+        if self.ranks[x] == self.ranks[y]:
+            self.clusters[y] = x
+        else:
+            self.clusters[x] = y
+        if self.ranks[x] == self.ranks[y]:
+            self.ranks[y] += 1
+        pass
