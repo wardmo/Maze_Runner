e181dd3e83846e87dbb442fc5b3439fbf4405f11
Morgan Ward <mward@powerteq.com>
2023-08-07 07:58:21 -0600

    more vestigial code dead

--- Maze_Types.py
+++ Maze_Types.py
@@ -6,15 +6,3 @@ class Maze_Types(Enum):
     Start = 2
     End = 3
     EdgeSet = set[tuple[tuple[int,int],tuple[int,int]]]
-
-class Maze_Node():
-    def __init__(self, id: tuple[int,int]):
-        self.data = None
-        self.id: tuple[int,int] = id
-        self.top: Maze_Edge = Maze_Edge()
-
-class Maze_Edge():
-    def __init__(self, id: tuple[tuple[int,int], tuple[int,int]]):
-        self.type: Maze_Types = Maze_Types.Wall
-        self.id: tuple[tuple[int,int], tuple[int,int]] = id
-        self.reverse_id: tuple[tuple[int,int],tuple[int,int]] = (id[1],id[0])
