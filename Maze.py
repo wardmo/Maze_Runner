f10694bc823edb3296fe7f248fccdcf78894b1d5
Morgan Ward <mward@powerteq.com>
2023-08-16 08:44:32 -0600

    set the solution_nodes as part of the rep class and defined a set_solution method that will go reassign solution nodes and edges in the rep as Maze_Type.Solution

--- Maze.py
+++ Maze.py
@@ -3,7 +3,7 @@ from Protocols import Maze
 
 class Maze_Rep():
 
-    def __init__(self, maze: Maze):
+    def __init__(self, maze: Maze, solution_nodes=set()):
         wall = Maze_Types.Wall
         path = Maze_Types.Path
         rep = [[wall for i in range(2*maze.width+1)] for j in range(2*maze.height+1)]
@@ -13,7 +13,7 @@ class Maze_Rep():
             j,i = node
             repj = 2*j+1
             repi = 2*i+1
-            if node in maze.solution_nodes:
+            if node in solution_nodes:
                 rep[repj][repi] = Maze_Types.Solution
             else:
                 rep[repj][repi] = path
@@ -25,13 +25,13 @@ class Maze_Rep():
             edgei = 2*i+1
             if i == ii:
                 edgej = 2*min(j,jj)+2
-                if n1 in maze.solution_nodes and n2 in maze.solution_nodes:
+                if n1 in solution_nodes and n2 in solution_nodes:
                     rep[edgej][edgei] = Maze_Types.Solution
                 else:
                     rep[edgej][edgei] = path
             elif j == jj:
                 edgei = 2*min(i,ii)+2
-                if n1 in maze.solution_nodes and n2 in maze.solution_nodes:
+                if n1 in solution_nodes and n2 in solution_nodes:
                     rep[edgej][edgei] = Maze_Types.Solution
                 else:
                     rep[edgej][edgei] = path
@@ -41,6 +41,27 @@ class Maze_Rep():
 
 
         pass
+
+    def set_solution(self, solution_nodes):
+        for node in solution_nodes:
+            j,i = node
+            repj = 2*j+1
+            repi = 2*i+1
+            self.content[repj][repi] = Maze_Types.Solution
+            adj_nodes = {
+                (j,i+1), (j,i-1), (j-1,i), (j+1,i)
+            }
+            for adj_node in adj_nodes:
+                if adj_node in solution_nodes:
+                    jj,ii = adj_node
+                    if ii == i:
+                        newrepj = 2*min(j,jj)+2
+                        if self.content[newrepj][repi] == Maze_Types.Path:
+                            self.content[newrepj][repi] = Maze_Types.Solution
+                    elif jj == j:
+                        newrepi = 2*min(i,ii)+2
+                        if self.content[repj][newrepi] == Maze_Types.Path:
+                            self.content[repj][newrepi] = Maze_Types.Solution
 
     @staticmethod
     def print_colored_block(text, color, width=1):
--- README.md
+++ README.md
@@ -17,12 +17,5 @@ print(Maze_Rep(maze))
 I want to have a better visualization solution, printing to the console has certain limits
 
 I want to refactor the solvers and mazes to follow the Protocols a little more closely.  
-I think the solution_nodes should belong to the Solver classes instead of the maze classes  
-I would rather have the usage be something like:  
-maze = DFS_Generation(height, width)  
-rep = Maze_Rep(maze)  
-print(rep)  
-solution = BFS(maze).solve()  
-print(Maze_Rep(maze, solution=solution))  
 
 enabling printing a maze before and after solving without having to create a whole new Maze_Rep object
